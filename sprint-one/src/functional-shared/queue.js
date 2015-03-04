97f60194ffce24241aa657c81f949d721b1d1287
MKS-Student <mks.student@hackreactor.com>
2015-03-04 15:57:43 -0800

    completed stack and queue for functional shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,39 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var results = {
+  	indexCounter : 0,
+  	storage : {}
+  };
+  _.extend(results, queueMethods);
+  return results;
 };
 
-var queueMethods = {};
+var queueMethods = {
+	enqueue : function(value) {
+		//var temp = this.storage[this.indexCounter];
+		var tempIndex = this.indexCounter;
+		while(tempIndex >= 0){
+			this.storage[tempIndex] = this.storage[tempIndex - 1];
+			tempIndex--;
+		}
+		this.storage[0] = value;
+		this.indexCounter++;
+	},
+
+	dequeue : function() {
+		if (this.indexCounter > 0){
+			this.indexCounter--;
+			var temp = this.storage[this.indexCounter];
+			delete this.storage[this.indexCounter];
+			return temp;
+		}
+	},
+
+	size : function() {
+		return this.indexCounter;
+	}
+};
 
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+ 
+  var bigStack = {
+  	indexCounter: 0,
+  	storage: {}
+  };
+  _.extend(bigStack, stackMethods);
+  return bigStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+push : function(value){
+		this[this.indexCounter] = value;
+		this.indexCounter++;
+},
 
+pop : function(){
+	if(this.indexCounter>0){
+			this.indexCounter--;
+			var results = this[this.indexCounter];
+			delete this[this.indexCounter];
+			return results;
+		}
+
+},
+size : function(){
+	 return this.indexCounter;
+}
+
+};
