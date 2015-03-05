6d96374c13ba8715fa6d01a3daffd29501a041f1
MKS-Student <mks.student@hackreactor.com>
2015-03-05 11:16:48 -0800

    Linked list completed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = new Node(value);
+    if (this.tail) {
+      this.tail.next = newNode;
+    }    
+    this.tail = newNode;
+    list.head ? list.head : list.head = newNode; 
   };
 
   list.removeHead = function(){
+    if (this.head) {
+      var tempHead = this.head;
+      this.head = this.head.next;
+      return tempHead.value;
+    }
   };
 
   list.contains = function(target){
+    var isFound = false;
+    //var testNode = this.head;
+    var checkValue = function(testNode) {
+      if (testNode.value === target) {
+        isFound = true;
+      }
+      if (testNode.next) { checkValue(testNode.next); };
+    };
+
+    checkValue(this.head);
+    return isFound;
   };
 
   return list;
