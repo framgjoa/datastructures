d362131d09218b48b8aa3d57804c0dd48e3a5956
MKS-Student <mks.student@hackreactor.com>
2015-03-05 09:51:33 -0800

    Queue completed

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.indexCounter = 0;
+  this.storage = {};
 };
 
+Queue.prototype.size = function() {
+	return this.indexCounter;
+};
 
+Queue.prototype.dequeue = function() {
+	if (this.indexCounter > 0) {
+		this.indexCounter--;
+		var temp = this.storage[this.indexCounter];
+		delete this.storage[this.indexCounter];
+		return temp;
+	}
+};
+
+Queue.prototype.enqueue = function(value) {
+	var tempCounter = this.indexCounter;
+	while(tempCounter > 0){
+		this.storage[tempCounter] = this.storage[tempCounter - 1];
+		tempCounter--;
+	}
+	this.storage[0] = value;
+	this.indexCounter++;
+}
+
+var output = new Queue();
