d19b555ca981771220319f14af0475ca01274900
MKS-Student <mks.student@hackreactor.com>
2015-03-05 09:58:01 -0800

    Stack completed

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.indexCounter = 0;
+  this.storage = {};
+
 };
 
+Stack.prototype.size = function(){
+	return this.indexCounter;
+};
+
+Stack.prototype.push = function(value){
+	this.storage[this.indexCounter] = value;
+	this.indexCounter++;
+};
+
+Stack.prototype.pop = function(){
+	if(this.indexCounter>0){
+		this.indexCounter--;
+		var temp = this.storage[this.indexCounter];
+		delete this.storage[this.indexCounter];
+		return temp;
+	}
+
+};
+
+var newStack = new Stack();
 
