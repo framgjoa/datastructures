fcf8ddc86602d5f8c9cab8e8c18e7a63e00e3451
MKS-Student <mks.student@hackreactor.com>
2015-03-05 16:38:00 -0800

    Graph completed, specs updated

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,51 @@
 
 
 var Graph = function(){
+	this.nodeHome = {};
 };
 
 Graph.prototype.addNode = function(node){
-};
+	var newNode = {
+	 	connections : []
+	};
 
-Graph.prototype.contains = function(node){
-};
-
-Graph.prototype.removeNode = function(node){
-};
-
-Graph.prototype.hasEdge = function(fromNode, toNode){
+	this.nodeHome[node] = newNode;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+	this.nodeHome[fromNode].connections.push(toNode);
+	this.nodeHome[toNode].connections.push(fromNode);
+};
+
+Graph.prototype.contains = function(node){
+	var isFound = false;
+
+	if (node in this.nodeHome) {
+		isFound = true;
+	}
+	
+	return isFound;
+};
+
+Graph.prototype.hasEdge = function(fromNode, toNode){
+	return this.nodeHome[fromNode].connections.indexOf(toNode) >= 0 ? true : false;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+	var tempIndex = this.nodeHome[fromNode].connections.indexOf(toNode);
+	this.nodeHome[fromNode].connections.splice(tempIndex, 1);
+	var tempIndex = this.nodeHome[toNode].connections.indexOf(fromNode);
+	this.nodeHome[toNode].connections.splice(tempIndex, 1);
+};
+
+Graph.prototype.removeNode = function(node){
+	delete this.nodeHome[node];
 };
 
 Graph.prototype.forEachNode = function(cb){
+	for (var key in this.nodeHome) {
+		cb(key);
+	}
 };
 
 /*
